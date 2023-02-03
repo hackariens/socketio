e1008efd2b49d9b92bb73505cacc9801f6e6228b
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-03 09:32:13 +0100

    fix(config): update socket io

--- apps/src/index.js
+++ apps/src/index.js
@@ -1,7 +1,18 @@
-var io = require('socket.io').listen(80);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-io.sockets.on('connection', function (socket) {
-  socket.on('ping', function (data) {
-    socket.emit("pong", 'pong');
-  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
